38699ad266a0328b71a93245aa156e3ff36f9968
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2022-03-08 19:49:52 +0100

    Update games_count.sh

--- scripts/games_count.sh
+++ scripts/games_count.sh
@@ -5,12 +5,18 @@
 #
 saves_dir="${HOME}/saves"
 count=0
+goal=0
 
 # Print config and gamefile count
 for folder in ${saves_dir}/*/
 do
+    # Count files
     files=$(ls ${folder}/*.pgn 2> /dev/null | wc -l || 0)
-    count=$(expr $count + $files)
+    count=$(($count + $files))
+    # Find goal count
+    pairs=$(grep -oP '(?<=pair 1 of )[0-9]{1,}' ${folder}/container.log | head -1)
+    rounds=$(grep -oP '(?<=game round 1 of )[0-9]{1,}' ${folder}/container.log | head -1)
+    goal=$(( ($goal + $pairs) * $rounds))
 done
 
 player=$(ls -t ${saves_dir}/*/*.pgn | head -1 | xargs grep White | cut -d'"' -f 2)
@@ -19,5 +25,6 @@ opponent=$(ls -t ${saves_dir}/*/*.pgn | head -1 | xargs grep Black | cut -d'"' -
 if [[ -z "${player}" ]]; then
     echo "0:${count}:Last game: None"
 else
-    echo "0:${count}:Last game: ${player} vs ${opponent}"
+    percent=$(( ($count * 100) / $goal))
+    echo "0:${count}:Last game: ${player} vs ${opponent}; ${count} of ${goal} games (${percent}%)"
 fi
